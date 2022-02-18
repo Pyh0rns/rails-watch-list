dfc3a13b6e68efaddbefd79aee5a701543cd92e5
PierreYves Hoorens <pyhoorens@gmail.com>
2022-02-18 12:51:11 +0100

    suppr links and btns ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
-  resources :lists do
+  resources :lists, except: :destroy do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
+  resources :lists, only: [:destroy], as: :list_destroy
 end
